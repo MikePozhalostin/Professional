2eed062d968cbca1a494be47453a2f7f5bac8286
Михаил Пожалостин <m.pozhalostin@whyhappen.ru>
2025-11-10 17:26:17 +0300

    Выполнение домашней работы на тему распараллеливание потоков с помощью task

---
+++ ParallelReadFiles/Program.cs
@@ -0,0 +1,23 @@
+﻿using ParallelReadFiles;
+using System.Diagnostics;
+
+var projectDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\..\files\"));
+
+var sw = new Stopwatch();
+
+sw.Start();
+Console.WriteLine($"Start read files from {projectDirectory}");
+
+try
+{
+    await ReadFilesHelper.GetWhiteSpacesInFilesFromDirectoryAsync(projectDirectory, CancellationToken.None);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.ToString());
+}
+finally
+{
+    sw.Stop();
+    Console.WriteLine($"Result: {sw.ElapsedMilliseconds} ms");
+}
---
+++ ParallelReadFiles/ReadFilesHelper.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections.Concurrent;
+
+namespace ParallelReadFiles
+{
+    internal static class ReadFilesHelper
+    {
+        const string searchPattern = "*.txt";
+
+        /// <summary>
+        /// Получить количество пробелов в файле
+        /// </summary>
+        /// <param name="directoryPath">Путь до папки с файлами</param>
+        /// <returns>Список путей к файлам и количество пробелов в них</returns>
+        internal async static Task<ConcurrentDictionary<string, int>> GetWhiteSpacesInFilesFromDirectoryAsync(string directoryPath, CancellationToken cancellationToken)
+        {
+            ArgumentNullException.ThrowIfNull(directoryPath);
+
+            if (!Directory.Exists(directoryPath)) {
+                Console.WriteLine("Папка не существует.");
+                throw new DirectoryNotFoundException(directoryPath);
+            }
+
+            var result = new ConcurrentDictionary<string, int>();
+
+            var files = Directory.GetFiles(directoryPath, searchPattern);
+
+            if (files.Length > 0)
+            {
+                Console.WriteLine($"В папке найдено {files.Length} файлов.");
+
+                var tasks = new List<Task<KeyValuePair<string, int>>>();
+
+                foreach (var file in files)
+                {
+                    tasks.Add(ReadWhiteSpacesFromFileAsync(file, cancellationToken));
+                }
+
+                while (tasks.Count > 0 || cancellationToken.IsCancellationRequested)
+                {
+                    try
+                    {
+                        var completedTask = await Task.WhenAny(tasks);
+
+                        if (!completedTask.IsFaulted && result.TryAdd(completedTask.Result.Key, completedTask.Result.Value))
+                        {
+                            Console.WriteLine($"Получены результаты подсчёта пробелов в файле {completedTask.Result.Key}. Количество: {completedTask.Result.Value}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Результаты подсчёта пробелов в файле {completedTask.Result.Key} не получены");
+                        }
+
+                        tasks.Remove(completedTask);
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex); }
+                }
+
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Файлы по заданному пути не найдены.");
+                return result;
+            }
+        }
+
+        private async static Task<KeyValuePair<string, int>> ReadWhiteSpacesFromFileAsync(string filePath, CancellationToken cancellationToken)
+        {
+            return await Task.Run(() =>
+            {
+                var spaceCount = 0;
+                using (var reader = new StreamReader(filePath))
+                {
+                    string? line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        spaceCount += line.Count(c => c == ' ');
+                    }
+                }
+                return new KeyValuePair<string, int> (filePath, spaceCount);
+            }, cancellationToken);
+        }
+    }
+}
